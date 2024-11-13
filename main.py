1033d9020824998693524a4d6e81622ef5eab435
Akash Debnath <109974358+akashD15@users.noreply.github.com>
2024-11-13 09:31:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+Find the number of elements which are less than the average.
+'''
+
+
+n = int(input())
+arr = input()
+l = list(map(int, arr.split(' ')))
+count = 0
+avg = sum(l)/len(l)
+for i in range(0,len(l)):
+    if(l[i]<avg):
+        count += 1
+        
+print (count)
